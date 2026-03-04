07eda487727528c89dad9aefde0e3c6b1bbc3cad
luki-crypto <lukihdgamer@gmail.com>
2026-03-04 18:17:15 +0000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist'
+  },
+  esbuild: {
+    loader: 'jsx',
+    include: /src\/.*|\.html$/,
+    exclude: []
   }
 })
